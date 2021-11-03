92d29901689910a6abc6a57ce8887532d1b8f26c
François FAUJEAU <ffaujeau@ipsosenso.com>
2021-11-03 14:11:44 +0100

    Style header

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,7 +23,7 @@ describe('AppComponent', () => {
   it(`should have as title 'flf'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('flf');
+    expect(app.title).toEqual('flfr');
   });
 
   it('should render title', () => {
