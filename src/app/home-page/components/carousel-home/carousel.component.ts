f9bac2d4518f0ba5fe3c0c1e822efc70704ff669
Francois Faujeau <ffaujeau@ipsosenso.com>
2022-03-09 14:04:11 +0100

    Dynamic carousel + style header fix alignement

--- src/app/home-page/components/carousel-home/carousel.component.ts
+++ src/app/home-page/components/carousel-home/carousel.component.ts
@@ -1,4 +1,6 @@
 import {Component} from '@angular/core';
+import {CarouselInfos} from '@flfr-app/home-page/models/CarouselInfos';
+import {BrowserUtils} from '@flfr-app/utils/BrowserUtils';
 
 @Component({
   selector: 'app-carousel',
@@ -7,6 +9,24 @@ import {Component} from '@angular/core';
 })
 export class CarouselComponent {
 
+  carouselInfos: CarouselInfos[] = [];
+  userAgentMobile = false;
+
   constructor() {
+    this.userAgentMobile = BrowserUtils.isMobileDevice();
+
+    if (!this.userAgentMobile) {
+      this.carouselInfos = [
+        new CarouselInfos('../../../../assets/images/testImg.jpg', 'Les fleurs des champs', 'C\'est jolie et ça sent bon.'),
+        new CarouselInfos('../../../../assets/images/testImg.jpg', 'Les fleurs des champs', 'Ca favorise le renouvellement de merveilleux écosystèmes.'),
+        new CarouselInfos('../../../../assets/images/testImg.jpg', 'Les fleurs des champs', 'Ca se mange mais c\'est pas très bon.')
+      ];
+    } else {
+      this.carouselInfos = [
+        new CarouselInfos('../../../../assets/images/uriel-WS4JcpoZz6E-unsplash.jpg', 'Les fleurs des champs', 'C\'est jolie et ça sent bon.'),
+        new CarouselInfos('../../../../assets/images/uriel-WS4JcpoZz6E-unsplash.jpg', 'Les fleurs des champs', 'Ca favorise le renouvellement de merveilleux écosystèmes.'),
+        new CarouselInfos('../../../../assets/images/uriel-WS4JcpoZz6E-unsplash.jpg', 'Les fleurs des champs', 'Ca se mange mais c\'est pas très bon.')
+      ];
+    }
   }
 }
---
+++ src/app/home-page/models/CarouselInfos.ts
@@ -0,0 +1,11 @@
+export class CarouselInfos {
+  imgPath = '';
+  text1 = '';
+  text2 = '';
+
+  constructor(imgPath: string, text1: string, text2: string) {
+    this.imgPath = imgPath;
+    this.text1 = text1;
+    this.text2 = text2;
+  }
+}
