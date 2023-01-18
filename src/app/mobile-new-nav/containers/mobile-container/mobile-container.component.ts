a9feffbcd00c46e649d626fca0067b95009ce236
franfauj98 <francois.faujeau@gmail.com>
2023-01-18 21:58:14 +0100

    refonte mobile: revue créa

--- src/app/mobile-new-nav/containers/mobile-container/mobile-container.component.ts
+++ src/app/mobile-new-nav/containers/mobile-container/mobile-container.component.ts
@@ -1,7 +1,5 @@
 import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
-import {fromEvent} from "rxjs";
 import {DomService} from "@flfr-app/services/dom.service";
-import {debounceTime, distinctUntilChanged} from "rxjs/operators";
 
 @Component({
   selector: 'app-mobile-container',
@@ -13,68 +11,46 @@ export class MobileContainerComponent implements OnInit {
   @ViewChild('two') divTwo!: ElementRef;
   @ViewChild('three') divThree!: ElementRef;
 
-  test1 = false;
-  test2 = false;
-  test3 = false;
-  test4 = false;
+  public displayHeaderChoice = false;
 
   constructor(private _domService: DomService) {
   }
 
   ngOnInit(): void {
-    const onScroll = fromEvent(window, 'scroll');
-    let lastY = 0;
-    onScroll
-      .subscribe(scroll => {
-        if (lastY > document.documentElement.scrollTop) {
-          // On scroll vers le haut
-          if (this.divTwo.nativeElement.getBoundingClientRect().y > 0 && this.divTwo.nativeElement.getBoundingClientRect().y <= this.divTwo.nativeElement.getBoundingClientRect().height) {
-            this.test2 = false;
-            this.test3 = false;
-            this.test4 = false;
-            if (!this.test1) {
-              console.log(1)
-              this.divOne.nativeElement.scrollIntoView()
-              this.test1 = true;
+    /*
+        const onScroll = fromEvent(window, 'scroll');
+        let lastY = 0;
+        onScroll
+          .pipe(debounceTime(50),
+            distinctUntilChanged())
+          .subscribe(scroll => {
+            if (lastY > document.documentElement.scrollTop) {
+              // On scroll vers le haut
+              if (this.divTwo.nativeElement.getBoundingClientRect().y > 0 && this.divTwo.nativeElement.getBoundingClientRect().y <= this.divTwo.nativeElement.getBoundingClientRect().height) {
+                this.divOne.nativeElement.scrollIntoView()
+              }
+              if (this.divThree.nativeElement.getBoundingClientRect().y > 0 && this.divThree.nativeElement.getBoundingClientRect().y <= this.divThree.nativeElement.getBoundingClientRect().height) {
+                this.divTwo.nativeElement.scrollIntoView()
+              }
+            } else {
+              if (+this.divTwo.nativeElement.getBoundingClientRect().y > 0 && +this.divTwo.nativeElement.getBoundingClientRect().y < this.divTwo.nativeElement.getBoundingClientRect().height) {
+                this.divTwo.nativeElement.scrollIntoView()
+              }
+              if (+this.divThree.nativeElement.getBoundingClientRect().y > 0 && +this.divThree.nativeElement.getBoundingClientRect().y < this.divThree.nativeElement.getBoundingClientRect().height) {
+                this.divThree.nativeElement.scrollIntoView()
+              }
             }
-          }
-          if (this.divThree.nativeElement.getBoundingClientRect().y > 0 && this.divThree.nativeElement.getBoundingClientRect().y <= this.divThree.nativeElement.getBoundingClientRect().height) {
-            this.test1 = false;
-            this.test3 = false;
-            this.test4 = false;
-            if (!this.test2) {
-              console.log(2)
-              this.divTwo.nativeElement.scrollIntoView()
-              this.test2 = true;
-            }
-          }
-        } else {
-          if (+this.divTwo.nativeElement.getBoundingClientRect().y > 0 && +this.divTwo.nativeElement.getBoundingClientRect().y < this.divTwo.nativeElement.getBoundingClientRect().height) {
-            this.test1 = false;
-            this.test2 = false;
-            this.test4 = false;
-            if (!this.test3) {
-              console.log(3)
-              this.divTwo.nativeElement.scrollIntoView()
-              this.test3 = true;
-            }
-          }
-          if (+this.divThree.nativeElement.getBoundingClientRect().y > 0 && +this.divThree.nativeElement.getBoundingClientRect().y < this.divThree.nativeElement.getBoundingClientRect().height) {
-            this.test1 = false;
-            this.test2 = false;
-            this.test3 = false;
-            if (!this.test4) {
-              console.log(4)
-              this.divThree.nativeElement.scrollIntoView()
-              this.test4 = true;
-            }
-          }
-        }
-        lastY = document.documentElement.scrollTop;
-      })
+            lastY = document.documentElement.scrollTop;
+          })
+    */
   }
 
-  test() {
-    this.divThree.nativeElement.scrollIntoView({behavior: 'instant', block: 'center'})
+  displayHeader(): void {
+    this.displayHeaderChoice = !this.displayHeaderChoice;
+  }
+
+  goToScreen(screen: ElementRef): void {
+    this.displayHeader();
+    screen.nativeElement.scrollIntoView();
   }
 }
